71d31e90a1c6d1a41508aaf226ad3ec76403f364
mini <155148431+mini0192@users.noreply.github.com>
2024-08-19 00:40:36 +0900

    Fix: build error

--- src/test/java/com/project/project/ProjectApplicationTests.java
+++ src/test/java/com/project/project/ProjectApplicationTests.java
@@ -3,7 +3,6 @@ package com.project.project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ProjectApplicationTests {
 
 	@Test
